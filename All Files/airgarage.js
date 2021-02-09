c865601d5b13c40d7c0479235619e0932bcd5f3f
Pooran Suthar <poorancoc@gmail.com>
2021-02-09 11:15:47 +0530

    Airgarage Project

---
+++ All Files/airgarage.js
@@ -0,0 +1,15 @@
+function demobutton(){
+    document.location.href = "demo.html";
+}
+function loginbutton(){
+    document.location.href = "login.html";
+}
+function findparking(){
+    document.location.href = "find_parking.html";
+}
+function signup(){
+    document.location.href = "singup.html";
+}
+function aftersignup(){
+    document.location.href = "aftersignup.html";
+}
